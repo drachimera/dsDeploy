7ba623170d6151b557d5f9ecfdc4d6295e70052a
M102417 <quest.daniel@mayo.edu>
2017-08-17 16:40:17 -0500

    making changes to flaskify

--- python/simplifiedSNF.py
+++ python/simplifiedSNF.py
@@ -1,8 +1,26 @@
+import sys
+import json
+import requests
+from flask import Flask, jsonify, request
 from sklearn.externals import joblib
 from sklearn.pipeline import make_pipeline
 
+app = Flask(__name__)
+
+@app.route('/run', methods = ['POST'])
+def run():
+    return "415 Unsupported Media Type ;)"
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    """
+    Create a heartbeat function that tests if the ml function is actually working properly,
+    in a production app, this could be an entire unit test suite that would test all boundary conditions for the model
+    """
+    return "Server is Up and Running!"
+
+
 def readmit_proba(prior_util_6m_ip_count=0, index_los=0, ch_index=0, icu_flag='n', lab_crea_resultn=0.0):
-    
     X = locals()
     X = X.values()
     if X[3] == 'n':
@@ -15,7 +33,6 @@ def readmit_proba(prior_util_6m_ip_count=0, index_los=0, ch_index=0, icu_flag='n
     return gbm.predict_proba(X)[0][1]
 
 def readmit(prior_util_6m_ip_count=0, index_los=0, ch_index=0, icu_flag='n', lab_crea_resultn=0.0):
-    
     X = locals()
     X = X.values()
     if X[3] == 'n':
@@ -26,3 +43,5 @@ def readmit(prior_util_6m_ip_count=0, index_los=0, ch_index=0, icu_flag='n', lab
     gbm = joblib.load('simplifiedSNF_gbm.pkl')
     
     return gbm.predict(X)[0]
+
+app.run(host='localhost', port= 8090)
